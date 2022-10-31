adae83617f6c14513009b27a228484a3875ad02c
malisit <masit@outlook.com>
2022-10-30 19:51:19 -0500

    new league result fixture

--- doksan/doksan.py
+++ doksan/doksan.py
@@ -84,44 +84,83 @@ class Doksan(object):
 			self.get_results_fixture_team_aux(soup, False)
 
 
-	def get_results_fixture_league_aux(self, soup, limit=2):
-		results = soup.find("div",{'id':'national'})
-		rounds = results.find_all(True, {'class':['ncet', 'blocks']})
-		num_rounds = 0
+	def get_results_fixture_league_aux(self, soup, res):
+		results = soup.find("div", {'class':'Ja'})
+		games = results.find_all("div")
+		
+		# print(games)
+		# text_file = open("sample.txt", "w")
+		# n = text_file.write(str(games[14]))
+		# text_file.close()
 
-		for line in rounds:
-			round_ = line.find("li", {'class': 'ncet_round'})
-			if round_:
-				num_rounds += 1
-				if num_rounds > limit:
-					break
-				print(round_.text)
-				
-			else:
-				game = line.find("tbody")
-				kick_date = game.find("span", {'class':'kick_t_dt'}).text
-				kick_time = game.find("span", {'class':'kick_t_ko'}).text
-				home = game.find("td", {'class':'home'}).text
-				away = game.find("td", {'class':'away'}).text
-				score = game.find("td", {'class':'score'}).text
+		results = {}
+		fixtures = {}
 
-				print(kick_date + " " + kick_time + " " + home + " " + score + " " + away)
+		for game in games:
+			class_ = game.get("class")
+			if class_:
+				class_ = " ".join(class_)
+				if class_ == "xb":
+					dt = game.find(True, {'class':'yb'}).text
+					# results.append({dt})
+					results[dt] = []
+					fixtures[dt] = []
+				if class_ == "Tc Xc":
+					time = game.find(True, {'class':'gg cg'}).text
+					sides = game.find_all(True, {'class':'jh'})
+					side1, side2 = sides[0].text, sides[1].text
+					side1_score = game.find(True, {'class':'mh'}).text
+					side2_score = game.find(True, {'class':'nh'}).text
+					# print(dt, time, side1,side1_score, side2_score, side2)
+					if time == "FT" and (res==0 or res==2):
+						results[dt].append(" ".join((time, side1,side1_score, "-", side2_score, side2)))
+						# print(time, side1,side1_score, " - ", side2_score, side2)
+					elif time !="FT" and (res==1 or res==2):
+						fixtures[dt].append(" ".join((time, side1, "-", side2)))
+						# print(time, side1, " - ", side2)
 
+		if res==0 or res!=1:
+			for r in results.items():
+				printed = False
+				if len(r[1])>0:
+					print(r[0])
+					last_dt = r[0]
+					printed = True
+				for game in r[1]:
+					print(game)
+
+				if printed:
+					print("")
+		if res==1 or res!=0:
+			for f in fixtures.items():
+				printed = False
+				if len(f[1])>0:
+					printed = True
+					if f[0] != last_dt:
+						print(f[0])
+						
+				for game in f[1]:
+					print(game)
+
+				if printed:
+						print("")
+						
 
 	def get_results_fixture_league(self):
 		league = self.league
-		
-		if self.results:
-			link = self.data[league]["_l_name_r"]
-			html = self.get_html(link)
-			soup = BeautifulSoup(html, features="html.parser")
-			self.get_results_fixture_league_aux(soup)
 
-		if self.fixture:
-			link = self.data[league]["_l_name_f"]
-			html = self.get_html(link)
-			soup = BeautifulSoup(html, features="html.parser")
-			self.get_results_fixture_league_aux(soup)
+		link = self.data[league]["_l_name"]
+		html = self.get_html(link)
+		soup = BeautifulSoup(html, features="html.parser")
+
+		if self.results and self.fixture:
+			self.get_results_fixture_league_aux(soup, 2)
+
+		if self.results and not self.fixture:
+			self.get_results_fixture_league_aux(soup, 0)
+
+		if self.fixture and not self.results:
+			self.get_results_fixture_league_aux(soup, 1)
 		
 
 	def get_league_table(self):
@@ -158,9 +197,6 @@ class Doksan(object):
 			l = [rank, team_, point, mp, wins, draw, lost, goals1, goals2, goals3]
 
 			print(" ".join(l))
-
-
-
 
 					
 
